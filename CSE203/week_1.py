b1b6015928425e936065ec5665a5826b8322e631
alveesarker <asahanaz305@gmail.com>
2025-02-08 15:48:03 +0600

    modified week one file

--- CSE203/week_1.py
+++ CSE203/week_1.py
@@ -1,1 +1,22 @@
 #toph problems---
+# Copycat
+# a = input()
+# print(int(a))
+
+# a = input()
+# b = a.split()
+# c, d = map(int, b)
+# print(c + d)
+
+
+# Thought Game
+def avrg(a, b):
+    return (a + b) // 2
+
+t = int(input())
+for i in range(t):
+    a, b = map(int, input().split())
+    if avrg(a, b) % 2 == 0:
+        print("Sadia will be happy.")
+    else:
+        print("Oops!")
