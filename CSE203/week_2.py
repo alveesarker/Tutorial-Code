f6aa2822c5627e1ce3665883c0bf130518356486
alveesarker <asahanaz305@gmail.com>
2025-02-21 19:43:27 +0600

    solved clock math problem

--- CSE203/week_2.py
+++ CSE203/week_2.py
@@ -29,7 +29,19 @@ else:
 """
 
 # Proper Leap Years
+"""
 year = int(input())
 if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
     print("Yes")
 else:print("No")
+"""
+
+# Clock Math
+H, M = map(int, input().split())
+"""
+angle = abs((30 * H - (11 / 2) * M))
+if angle > 180:
+    angle = 360 - angle
+
+print(f"{angle:.7f}")
+"""
