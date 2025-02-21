69f7fd00a89aa7d40d6b498eca1f74ce3af8e2ea
alveesarker <asahanaz305@gmail.com>
2025-02-21 19:23:32 +0600

    solved Leap yars problem

--- CSE203/week_2.py
+++ CSE203/week_2.py
@@ -1,14 +1,27 @@
 # Missing Number
+"""
 x = int(input())
 a, b, c = map(int, input().split())
 done = a + b + c
 d = x - done
 print(d)
+"""
 
 # Mixed Fractions
+"""
 a, b = map(int, input().split())
 print(a//b, a % b, b)
+"""
 
 # Math and Watermelons
+"""
 m, k = map(int, input().split())
 print(m % k)
+"""
+
+# Leap Years
+year = int(input())
+if year % 4 == 0 and year % 400 != 0:
+    print("Yes")
+else:
+    print("No")
