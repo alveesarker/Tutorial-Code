229e006385780d64d2bed0f74263f8cea78df98e
alveesarker <asahanaz305@gmail.com>
2025-03-08 11:27:25 +0600

    week 4

--- CSE203/week_4.py
+++ CSE203/week_4.py
@@ -1,6 +1,26 @@
 # Black or White
-A, B = map(str, input().split())
-if A == B:
-    print('Black')
+"""
+a, b = input().split()
+
+last_dig_a = int(a[-1])
+last_dig_b = int(b[-1])
+
+if (last_dig_a + last_dig_b) % 2 == 0:
+    print("Black")
 else:
-    print('White')
+    print("White")
+"""
+
+
+# Formatted Numbers
+num = input().strip()
+length = len(num)
+result = []
+
+for i in range(length):
+    if i > 0 and (length - i) % 3 == 0:
+        result.append(',')
+    result.append(num[i])
+
+formatted_number = ''.join(result)
+print(formatted_number)
