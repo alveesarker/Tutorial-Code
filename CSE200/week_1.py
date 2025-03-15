dfc26c8694730ebb32661ee646f5a79b9b23b79f
alveesarker <asahanaz305@gmail.com>
2025-03-15 21:06:49 +0600

    completed week one problems for cse200

--- CSE200/week_1.py
+++ CSE200/week_1.py
@@ -1,1 +1,13 @@
-print("Hello world")
+# Pattern Design
+def pattern_design(n):
+  print(" " * ((n *2) + 1) + "+" + ("-" * (n * 6)) + "+")
+  for i in range(n*2):
+    print(" " *( (n*2) - i) + "/" + " " * (((n * 2) - i - 1) * 3) + "___/" + "__/"  * i + "/" *i)
+  print("+" + ("-" * (n * 6)) + "+" + "/" * (n * 2))
+  for i in range(n):
+    print("|" + " " * (((n*6) - 21) // 2) + "How to Code in Python" + " " * ((((n * 6)-21) - ((n*6) - 21) // 2)) + "|" + "/" * ((n * 2) - (i * 2)))
+  print("+" + ("-" * (n * 6)) + "+")
+  print(f"Now only ${n * 10}!")
+pattern_design(5)
+
+
