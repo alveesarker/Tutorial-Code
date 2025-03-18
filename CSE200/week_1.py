fb6df8f005005004d3e7f31ba93ffe6436744077
alveesarker <asahanaz305@gmail.com>
2025-03-18 10:23:27 +0600

    completed week 3 problems

--- CSE200/week_1.py
+++ CSE200/week_1.py
@@ -5,7 +5,8 @@ def pattern_design(n):
     print(" " *( (n*2) - i) + "/" + " " * (((n * 2) - i - 1) * 3) + "___/" + "__/"  * i + "/" *i)
   print("+" + ("-" * (n * 6)) + "+" + "/" * (n * 2))
   for i in range(n):
-    print("|" + " " * (((n*6) - 21) // 2) + "How to Code in Python" + " " * ((((n * 6)-21) - ((n*6) - 21) // 2)) + "|" + "/" * ((n * 2) - (i * 2)))
+    print("|" + " " * (((n*6) - 21) // 2) + "How to Code in Python" + " " * (
+                ((n * 6) - 21) - ((n * 6) - 21) // 2) + "|" + "/" * ((n * 2) - (i * 2)))
   print("+" + ("-" * (n * 6)) + "+")
   print(f"Now only ${n * 10}!")
 pattern_design(5)
--- CSE203/week_3.py
+++ CSE203/week_3.py
@@ -1,14 +1,36 @@
-"""
+# First problem
+print("Hello World!")
+
 n = int(input())
 for i in range(n):
     if i == n - 1:
         print("*" + " *" * (n - 1))
     else:
         print(" " * (n - i -2) + " *" * (i + 1))
-"""
+
 
 n = float(input())
 n_int = int(n)
 perc = n_int // 10
 print("[" + "+" * perc + "." * (10 - perc) + "]" + " " + str(n_int) + "%")
 
+
+# Fair Distribution
+a, b = map(int, input().split())
+print(a - (b % a))
+
+
+# Maximum
+n = int(input())
+lst = map(int, input().split())
+print(max(lst))
+
+
+
+# Divisors
+n = int(input())
+print(1)
+for i in range(2, int(n/2) + 1):
+    if n % i == 0:
+        print(i)
+print(n)
