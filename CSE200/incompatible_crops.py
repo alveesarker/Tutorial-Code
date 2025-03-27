3837f6bbeccb6bf8eeb7db4e7a84dbefb2696684
alveesarker <asahanaz305@gmail.com>
2025-03-28 01:11:12 +0600

    completed the task

--- CSE200/incompatible_crops.py
+++ CSE200/incompatible_crops.py
@@ -1,10 +1,21 @@
 row = int(input())
 col = int(input())
-corps = [input().split() for _ in range(row)]
+corps = [list(input()) for _ in range(row)]
 count = 0
 
 for i in range(row):
     for j in range(col):
         if corps[i][j] == ".":
             mine = 0
-            if j != 0 and corps[]
+            if j != 0 and corps[i][j - 1] == "*":
+                mine += 1
+            if j != (col - 1) and corps[i][j + 1] == "*":
+                mine += 1
+            if i != 0 and corps[i - 1][j] == "*":
+                mine += 1
+            if i != (row - 1) and corps[i + 1][j] == "*":
+                mine += 1
+            if mine == 0:
+                count += 1
+
+print(count)
