c1fff8659049f8650c53498a948122306e2fda19
alveesarker <asahanaz305@gmail.com>
2025-03-30 16:23:05 +0600

    partially solved

--- CSE200/knapsack.py
+++ CSE200/knapsack.py
@@ -1,1 +1,10 @@
-print("Hello world!")
+a, b = map(int, input().split())
+bag = []
+
+for i in range(a):
+    value, weight = list(map(int, input().split()))
+    bag.append((value, weight, value/weight))
+
+bag.sort(key=lambda x: x[2], reverse=True)
+
+for i in range(len(bag)):
