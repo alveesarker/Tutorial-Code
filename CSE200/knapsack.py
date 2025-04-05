1e5a3d9775391593ce3bd8e1e2f7e05633462717
alveesarker <asahanaz305@gmail.com>
2025-04-05 08:34:35 +0600

    new commit

--- CSE200/knapsack.py
+++ CSE200/knapsack.py
@@ -5,6 +5,9 @@ for i in range(a):
     value, weight = list(map(int, input().split()))
     bag.append((value, weight, value/weight))
 
-bag.sort(key=lambda x: x[2], reverse=True)
+bag.sort(key=lambda x: x[2])
 
-for i in range(len(bag)):
+# w = 0
+# c = 0
+# for i in range(len(bag)):
+#     if w +
