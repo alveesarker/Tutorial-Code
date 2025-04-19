a316c9a7072318156a9e9704e00571123099b902
alveesarker <asahanaz305@gmail.com>
2025-04-19 08:46:40 +0600

    completed class and object

---
+++ CSE200/class_object.py
@@ -0,0 +1,19 @@
+class Student:
+    def __init__(self, f_name, l_name, id, cgpa, dept):
+        self.f_name = f_name
+        self.l_name = l_name
+        self.id = id
+        self.cgpa = cgpa
+        self.dept = dept
+
+    def fullname(self):
+        print(self.f_name + " " + self.l_name)
+
+s1 = Student("Riyad", "Hasan",  2238478, 2, "CSE")
+s2 = Student("Alvee", "Sarker", "2311249", 2, "CSE")
+
+# print(s1.id)
+# print(s1.l_name)
+s1.fullname()
+s2.fullname()
+
